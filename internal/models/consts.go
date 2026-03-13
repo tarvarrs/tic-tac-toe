35dab791eb448ca79c5f52509546347a89362a59
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2026-03-13 17:38:49 +0300

    reorganize project structure

--- internal/models/consts.go
+++
@@ -1,9 +0,0 @@
-package models
-
-const (
-	MaximizerFig = 1
-	MinimizerFig = -1
-	EmptyFig     = 0
-	WinScoreMax  = 3
-	WinScoreMin  = -3
-)
--- internal/models/grid.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type Grid struct {
-	Matrix [3][3]int
-}
